185b2edca86d85eb9a6d30c5752a96da5b47049d
stanislauy <stas.yudzitski@yandex.ru>
2019-10-02 16:51:37 +0300

    Nested links technique.

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React from 'react';
+import './styles.scss';
 
 const App = () => {
-  return <div>Rooot component</div>;
+  return (
+    <div>
+      <div>Rooot component</div>
+      <a href="#1">
+        Link content that references <br />
+        <a href="#2"> another link </a>
+        within the main one.
+      </a>
+      <div className="post-link">
+        <p>
+          Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum
+          tortor quam, <a href="https://www.google.com">feugiat vitae</a>, ultricies eget, tempor sit amet, ante.{' '}
+          <a href="http://www.apple.com/">Aenean fermentum</a>, elit eget tincidunt condimentum, eros ipsum rutrum orci,
+          sagittis tempus lacus enim ac dui. Ut felis. Praesent dapibus, neque{' '}
+          <a href="https://www.facebook.com/">id cursus faucibus</a>, tortor neque egestas augue, eu vulputate magna
+          eros eu erat. Aliquam erat volutpat.
+        </p>
+
+        <a href="http://csswizardry.com/2013/02/introducing-csswizardry-grids/" className="post-link__read-more">
+          Read more…
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default App;
