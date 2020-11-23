c815bf4a53cd9bbf4ae9bd5d8923ae5b5abb0f22
Stas <stannyu@MacBook-Pro-Stas.local>
2020-11-23 22:01:45 +0100

    Initial commit for VScode usage

---
+++ src/components/AnotherComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./cmp-styles"
+
+const AnotherComponent = () => {
+    return (
+        <div className="my-class">
+            Here you go - another one.
+        </div>
+    );
+};
+
+export default AnotherComponent;
--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
 import React from "react";
+import AnotherComponent from "./AnotherComponent";
 import ChildComponent from './ChildComponent';
 
 const App = () => {
+  console.log("Hell ya!")
   return <>
     <div>Root component</div>
+    <h1>Hi there</h1>  
+    <AnotherComponent />
     <ChildComponent text={'Outer Space'}/>
   </>;
 };
